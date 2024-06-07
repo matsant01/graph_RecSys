minor fixes to metrics computation

src/models.py
@@ -416,68 +416,72 @@ class GNN(torch.nn.Module):
             print(len(batch))
             print(f"\nEpoch {epoch + 1}/{num_epochs} - Train Loss: {float(loss)}")
         
-            ######################## Validate the model ########################
-            # Compute validation loss
-            avg_val_loss, predictions  = self.evaluation_full_batch(val_data, device, criterion)
-            print(f"Epoch {epoch + 1}/{num_epochs} - Validation Loss: {avg_val_loss}")
-            
-            if writer is not None:
-                writer.add_scalar(
-                    tag="val/loss",
-                    scalar_value=avg_val_loss,
-                    global_step=epoch
-                )
+            if epoch % val_steps == 0:
+                ######################## Validate the model ########################
+                # Compute validation loss
+                avg_val_loss, predictions  = self.evaluation_full_batch(val_data, device, criterion)
+                print(f"Epoch {epoch + 1}/{num_epochs} - Validation Loss: {avg_val_loss}")
                 
-            # Extract most likely class if the model is a classifier
-            if self.is_classifier:
-                predictions = torch.cat(predictions, dim=0).argmax(dim=-1).cpu().numpy()
-            else:
-                predictions = torch.cat(predictions, dim=0).cpu().numpy()
-        
-        # Compute metrics
+                if writer is not None:
+                    writer.add_scalar(
+                        tag="val/loss",
+                        scalar_value=avg_val_loss,
+                        global_step=epoch
+                    )
+                    
+                # Extract most likely class if the model is a classifier
+                if self.is_classifier:
+                    predictions = torch.cat(predictions, dim=0).argmax(dim=-1).cpu().numpy()
+                else:
+                    predictions = torch.cat(predictions, dim=0).cpu().numpy()
+            
+                # Compute metrics
+                k = 5
+                threshold = 4
+                big_k = 10
 
-        k = 5
-        threshold = 4
-        map_k = 10
-
-        results_df = pd.DataFrame([
-            {
-                "user_id": int(user_id),
-                "book_id": int(book_id),
-                "rating": int(true_label),
-                "predicted_rating": int(predicted_label),
-            }
-            for user_id, book_id, true_label, predicted_label in zip(
-                val_data[("user", "rates", "book")].edge_label_index[0],
-                val_data[("user", "rates", "book")].edge_label_index[1],
-                val_data[("user", "rates", "book")].edge_label,
-                predictions,
-            )
-        ])
+                results_df = pd.DataFrame([
+                    {
+                        "user_id": int(user_id),
+                        "book_id": int(book_id),
+                        "rating": int(true_label),
+                        "predicted_rating": int(predicted_label),
+                    }
+                    for user_id, book_id, true_label, predicted_label in zip(
+                        val_data[("user", "rates", "book")].edge_label_index[0],
+                        val_data[("user", "rates", "book")].edge_label_index[1],
+                        val_data[("user", "rates", "book")].edge_label,
+                        predictions,
+                    )
+                ])
 
 
-        # Evaluate the recommendations  
+                # Evaluate the recommendations  
+                mean_precision, mean_recall, mean_f1, map_k = evaluate_recommendations(results_df, threshold, k, big_k)
+                print(f"Mean Precision@{k}: {mean_precision}")
+                print(f"Mean Recall@{k}: {mean_recall}")
+                print(f"Mean F1 Score@{k}: {mean_f1}")
+                print(f"Mean Average Precision@{big_k}: {map_k}")
+                if writer is not None:
+                    writer.add_scalar(
+                        tag=f"val/precision@{k}",
+                        scalar_value=mean_precision,
+                        global_step=epoch
+                    )
+                    writer.add_scalar(
+                        tag=f"val/recall@{k}",
+                        scalar_value=mean_recall,
+                        global_step=epoch
+                    )
+                    writer.add_scalar(
+                        tag=f"val/f1@{k}",
+                        scalar_value=mean_f1,
+                        global_step=epoch
+                    )
+                    writer.add_scalar(
+                        tag=f"val/map@{big_k}",
+                        scalar_value=map_k,
+                        global_step=epoch
+                    )
 
-        mean_precision, mean_recall, mean_f1, map_k = evaluate_recommendations(results_df, threshold, k, 10)
-        print(f"Mean Precision@{k}: {mean_precision}")
-        print(f"Mean Recall@{k}: {mean_recall}")
-        print(f"Mean F1 Score@{k}: {mean_f1}")
-        print(f"Mean Average Precision@{k}: {map_k}")
-        if writer is not None:
-            writer.add_scalar(
-                tag=f"val/precision@{k}",
-                scalar_value=mean_precision,
-                global_step=epoch
-            )
-            writer.add_scalar(
-                tag=f"val/recall@{k}",
-                scalar_value=mean_recall,
-                global_step=epoch
-            )
-            writer.add_scalar(
-                tag=f"val/f1@{k}",
-                scalar_value=mean_f1,
-                global_step=epoch
-            )
-
-        self.train()
+                self.train()