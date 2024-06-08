2047f3d061ba68bb75641e7faecbdc588c8d8fec
Matteo Santelmo <matteo.santelmo@epfl.ch>
2024-06-08 09:13:29 +0100

    More fixes

--- scripts/evaluator.py
+++ scripts/evaluator.py
@@ -15,13 +15,16 @@ from src.evaluation_metrics import *
 
 def load_model(model_folder, full_data, config):
     if "encoder_arch" in config: 
-        model = GNN(data=full_data,
-                    conv_hidden_channels=config['hidden_channels'],
-                    lin_hidden_channels=config['hidden_channels'],
-                    num_conv_layers=config['num_conv_layers'],
-                    num_decoder_layers=config['num_decoder_layers'],
-                    use_embedding_layers=config['use_embedding_layers'], 
-                    encoder_arch=config["encoder_arch"])
+        model = GNN(
+            data=full_data,
+            conv_hidden_channels=config['hidden_channels'],
+            lin_hidden_channels=config['hidden_channels'],
+            num_conv_layers=config['num_conv_layers'],
+            num_decoder_layers=config['num_decoder_layers'],
+            use_embedding_layers=config['use_embedding_layers'],
+            encoder_arch=config['encoder_arch'],
+            out_dim=1 if config["loss"] != "nll" else 5,   
+        )
     else: 
         model = GNN(data=full_data,
                     conv_hidden_channels=config['hidden_channels'],
@@ -37,48 +40,50 @@ def load_model(model_folder, full_data, config):
 
 def load_data(test_data,  config):
     # using same loader as during training
-    num_neighbors = [config['num_neighbors_in_sampling']] * config['num_iterations_loader']
-    if config['sampler_type'] == "link-neighbor":
-        test_loader =  LinkNeighborLoader(
-            data=test_data,
-            num_neighbors=num_neighbors,
-            neg_sampling_ratio=0,  # no negative sampling, otherwise number of prediction wouldn't match the number of labels
-            edge_label_index=(("user", "rates", "book"), test_data["user", "rates", "book"].edge_label_index),
-            edge_label=test_data["user", "rates", "book"].edge_label,
-            batch_size=config['batch_size'],
-            shuffle=True,
-        )
+    # num_neighbors = [config['num_neighbors_in_sampling']] * config['num_iterations_loader']
+    # if config['sampler_type'] == "link-neighbor":
+    #     test_loader =  LinkNeighborLoader(
+    #         data=test_data,
+    #         num_neighbors=num_neighbors,
+    #         neg_sampling_ratio=0,  # no negative sampling, otherwise number of prediction wouldn't match the number of labels
+    #         edge_label_index=(("user", "rates", "book"), test_data["user", "rates", "book"].edge_label_index),
+    #         edge_label=test_data["user", "rates", "book"].edge_label,
+    #         batch_size=config['batch_size'],
+    #         shuffle=True,
+    #     )
 
-    elif config['sampler_type']  == "HGT":
-        test_loader = HGTLoader(
-            test_data,
-            num_samples=num_neighbors,  
-            shuffle=True,
-            batch_size=config['batch_size'],
-            input_nodes=("user", None),
-        )
+    # elif config['sampler_type']  == "HGT":
+    #     test_loader = HGTLoader(
+    #         test_data,
+    #         num_samples=num_neighbors,  
+    #         shuffle=True,
+    #         batch_size=config['batch_size'],
+    #         input_nodes=("user", None),
+    #     )
         
-    return test_loader
+    # return test_loader
 
+    return test_data
 
 def compute_save_metrics(test_data, predictions, model_folder):
-    for k in [5, 10, 25]:
-        threshold = 4
-        map_k = 10
+    threshold = 4
+    K = 10
+    k = 5
 
-        test_data['predicted_rating'] = predictions
-        # Evaluate the recommendations  
-        mean_precision, mean_recall, mean_f1, map_k = evaluate_recommendations(test_data, threshold, k, 10)
-        print(f"Mean Precision@{k}: {mean_precision}")
-        print(f"Mean Recall@{k}: {mean_recall}")
-        print(f"Mean F1 Score@{k}: {mean_f1}")
-        print(f"Mean Average Precision@{k}: {map_k}")
+    test_data['predicted_rating'] = predictions
+    # Evaluate the recommendations  
+    mean_precision, mean_recall, mean_f1, map_k = evaluate_recommendations(test_data, threshold, k, K)
+    print(f"Mean Precision@{k}: {mean_precision}")
+    print(f"Mean Recall@{k}: {mean_recall}")
+    print(f"Mean F1 Score@{k}: {mean_f1}")
+    print(f"Mean Average Precision@{K}: {map_k}")
 
-        metrics = {
-            f"Mean Precision@{k}": mean_precision,
-            f"Mean Recall@{k}": mean_recall,
-            f"Mean F1 Score@{k}": mean_f1
-        }
+    metrics = {
+        f"Mean Precision@{k}": mean_precision,
+        f"Mean Recall@{k}": mean_recall,
+        f"Mean F1 Score@{k}": mean_f1,
+        f"Mean Average Precision@{K}": map_k
+    }
 
     with open(os.path.join(model_folder, 'metrics.json'), 'w') as f:
         json.dump(metrics, f, indent=4)
@@ -91,10 +96,9 @@ def evaluate_model_in_folder(model_folder, test_data, full_data, test_data_csv):
     test_loader = load_data(test_data, config)
 
     model = load_model(model_folder, full_data, config).to(device)
-    avg_loss, predictions = model.evaluation(test_loader, device)
+    _, predictions = model.evaluation_full_batch(test_loader, device, criterion=None)
 
     print(config)
-    predictions = torch.cat(predictions, dim=0).cpu().numpy()
     compute_save_metrics(test_data_csv, predictions, model_folder)
 
 
